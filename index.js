d4b1846cfc5ea99bd4e78be552617cf9977523df
Forsakenpanda <nicholas.gregorio@uoit.net>
2017-01-30 16:24:03 -0500

    Startup

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 6000));
+
+// views is directory for all template files
+app.set('www', __dirname + '/www');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
 
 app.listen(app.get('port'), function(){
     console.log("Test");
