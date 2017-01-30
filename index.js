a5fd8bb2651e531841411b466eff3f99a9b56971
Forsakenpanda <nicholas.gregorio@uoit.net>
2017-01-30 16:27:00 -0500

    Startup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 6000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.render('www/index');
