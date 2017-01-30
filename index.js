dd8ec775f1637b9924099afe1fdaa21af52460bd
Forsakenpanda <nicholas.gregorio@uoit.net>
2017-01-30 16:15:45 -0500

    Startup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
-app.listen(5001);
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'));
 
 app.get('/', function(res,req){
-    res.send("Hello World");
+    console.log('Node app is running on port', app.get('port'));
 });
